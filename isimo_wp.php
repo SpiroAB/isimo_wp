6f2046afc92258110a14812313a9c4ad401ff3d5
Thorbjörn Backlund <thorbjorn@spiro.se>
2018-01-19 14:11:00 +0100

    Isimo wp v0.0.4, support gitsha tags

--- isimo_wp.php
+++ isimo_wp.php
@@ -2,12 +2,12 @@
 	/**
 	 * @package Isimo Client
 	 * @author Puggan
-	 * @version 0.0.3
+	 * @version 0.0.4
 	 */
 	/*
 	Plugin Name: Isimo Client
 	Description: Providing data to the Isimo Server
-	Version: 0.0.3
+	Version: 0.0.4
 	Author: Puggan
 	*/
 
@@ -40,7 +40,7 @@
 		$data->report = [];
 		$data->software = 'Wordpress';
 		$data->version = $wp_version;
-		$data->client = 'isimo wp 0.0.3';
+		$data->client = 'isimo wp 0.0.4';
 
 		ob_start();
 		phpinfo();
@@ -50,6 +50,25 @@
 		foreach($wpdb->get_results('SHOW VARIABLES') as $row) {
 			$data->mysql[$row->Variable_name] = $row->Value;
 		};
+
+		$data->gitsha = null;
+		$git_dirs = ["{$_SERVER['DOCUMENT_ROOT']}/.git", ABSPATH . '/../.git', ABSPATH . '/.git'];
+		foreach($git_dirs as $git_dir)
+		{
+			if(!is_dir($git_dir)) continue;
+			if(!is_file($git_dir . '/HEAD')) continue;
+			$git_head = file_get_contents($git_dir . '/HEAD');
+			if(!$git_head) continue;
+			$git_head = trim(substr($git_head, 4));
+			if(!$git_head) continue;
+			if(!is_file($git_dir . '/' . $git_head)) continue;
+			$git_ref = file_get_contents($git_dir . '/' . $git_head);
+			if(!$git_ref) continue;
+			$git_ref = trim($git_ref);
+			if(!$git_ref) continue;
+			$data->gitsha = $git_ref;
+			break;
+		}
 
 		header('Content-Type: application/json');
 		echo json_encode($data, JSON_PRETTY_PRINT);
