ebefeb19451f5f3cf021914406e12a07167fd9ee
Thorbjörn Backlund <thorbjorn@spiro.se>
2018-05-28 16:22:24 +0200

    Genereate Tokens in many different php setups, try 5 different random functions

--- Isimo.php
+++ Isimo.php
@@ -63,8 +63,49 @@
 
 		public function generate_tooken()
 		{
-			/** @noinspection CryptographicallySecureRandomnessInspection */
-			return str_replace(array('+', '/'), array('-', '_'), base64_encode(openssl_random_pseudo_bytes(24)));
+			if(function_exists('random_bytes'))
+			{
+				/** @noinspection CryptographicallySecureRandomnessInspection */
+				return str_replace(array('+', ''), array('-', ''), base64_encode(random_bytes(24)));
+			}
+			if(function_exists('openssl_random_pseudo_bytes'))
+			{
+				/** @noinspection CryptographicallySecureRandomnessInspection */
+				return str_replace(array('+', ''), array('-', ''), base64_encode(openssl_random_pseudo_bytes(24)));
+			}
+			if(function_exists('mcrypt_create_iv'))
+			{
+				/** @noinspection CryptographicallySecureRandomnessInspection */
+				return str_replace(array('+', ''), array('-', ''), base64_encode(mcrypt_create_iv(24)));
+			}
+
+			if(function_exists('mt_rand'))
+			{
+				return implode(
+					'',
+					array_map(
+						function ($n) {
+							return base64_encode(hex2bin(substr('00000' . dechex(mt_rand(0, 0xFFFFFF)), -6)));
+						},
+						range(1, 6)
+					)
+				);
+			}
+
+			if(function_exists('rand'))
+			{
+				return implode(
+					'',
+					array_map(
+						function ($n) {
+							return base64_encode(hex2bin(substr('00000' . dechex(rand(0, 0xFFFFFF)), -6)));
+						},
+						range(1, 6)
+					)
+				);
+			}
+
+			return 'MsX3oFUobc8iWyDQ6JfPjk36MCIkNYLQ';
 		}
 
 		public function option_page()
